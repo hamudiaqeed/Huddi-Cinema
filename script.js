eb50a7f5a897387efd92f5aa4e021501b1040af6
Mohammed Aqeed <hamudy_fun5b@yahoo.com>
2021-12-23 19:06:50 +0200

    adding movies to the dom

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=f063e973cf
 
 const form = document.getElementById('form');
 const search = document.getElementById('search');
+const main = document.getElementById('main');
 
 getMovies(API_URL)
 
@@ -11,7 +12,39 @@ async function getMovies(url) {
     const res = await fetch(url)
     const data = await res.json()
 
+    showMovies(data.results)
+
     // console.log(data.results)
+}
+
+function showMovies(movies) {
+    main.innerHTML = ''
+
+    movies.map((movie) => {
+        main.innerHTML += `
+            <div class='movie'>
+                <img src="${IMG_PATH + movie.poster_path}" alt="${movie.title}">
+                <div class="movie-info">
+                    <h3>${movie.title}</h3>
+                    <span class="${getClassByRate(movie.vote_average)}">${movie.vote_average}</span>
+                </div>
+                <div class="overview">
+                <h3>Overview</h3>
+                    ${movie.overview}
+                </div>
+            </div>
+        `;
+    })
+}
+
+function getClassByRate(vote) {
+    if(vote >= 8) {
+        return 'green'
+    } else if(vote >= 5) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
 }
 
 form.addEventListener('submit', (e) => {
